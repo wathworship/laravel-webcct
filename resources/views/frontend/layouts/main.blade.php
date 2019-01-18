ca20f7004a59061fed740081a3e1133187b1048f
wath <wathworship@gmail.com>
2019-01-18 16:51:11 +0700

    jen18

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -12,24 +12,25 @@
             @include('frontend.partials._script')
             
            <!-- Load Facebook SDK for JavaScript -->
-            <div id="fb-root"></div>
-                <script>
-                    (function(d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-                    fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));
-                </script>
+           <div class="fb-customerchat" page_id="438782096583523"></div>
 
-                <!-- Your customer chat code -->
-                <div class="fb-customerchat"
-                attribution=setup_tool
-                page_id="438782096583523"
-                theme_color="#7646ff"
-                logged_in_greeting="สภาคริสตจักรในประเทศไทย ยินดีให้คำปรึกษา"
-                logged_out_greeting="สภาคริสตจักรในประเทศไทย ยินดีให้คำปรึกษา">
-            </div>
+            <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                appId            : '438782096583523',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v3.2'
+                });
+            };
+
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+                fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            </script>
         </body>
 </html>
